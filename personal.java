866d2dcdf8d3fc16547b5e5de6d7865b39235ec3
ramadhanisalama <95014953+ramadhanisalama@users.noreply.github.com>
2021-11-25 00:48:05 -0800

    Add files via upload

---
+++ personal.java
@@ -0,0 +1,10 @@
+public class Personal{
+    public static void main(String[] args) {
+        double phone = 0777010562;
+        System.out.println("salama");
+        System.out.println("Mtoni kidatu" + "Zanzibar" + "Mtoni" + 123 );
+        System.out.println(phone);
+        System.out.println("SUZA");
+        
+    }
+}
